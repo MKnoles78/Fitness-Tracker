4109db02040b27931c21ffb33f0085055e16800b
MKnoles <mknoles78@gmail.com>
2020-04-23 23:05:20 -0400

    was able to have virtual create total duration

--- models/workout.js
+++ models/workout.js
@@ -39,34 +39,32 @@
 
         }
       ]
-    });
+    },
+    
+    { toJSON: { virtuals: true } },
+    );
 
-const opts = { toJSON: { virtuals: true } };
-// const RecipeSchema = new Schema(
-//   {
-//     title: {
-//       type: String,
-//     },
-//     ingredients: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: "Ingredient",
-//       },
-//     ],
-//     timeToCook: {
-//       type: Number,
-//     },
-//     timeToPrepare: {
-//       type: Number,
-//     },
-//   },
-  opts
-// );
+// toJSON needed to be in the schema to insert virtual into the workoutSchema
+// once there you can take the total, reduce takes the end number value
+// we need the total of each exercise and to return the total amounts and the excercise duration
 
-workoutSchema.virtual("totalDuration").get(function () {
-  return this.exercises;
+workoutSchema.virtual("totalDuration").get(function(){
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration
+  }, 0);
 });
 
+// workoutSchema.virtual("totalDuration").get(function () {
+//   const tallied = exercises.reduce((acc, curr) => {
+//     if (curr.type === "resistance") {
+//       acc.totalDistance = (acc.totalDuration || 0) + curr.duration;
+//     } else if (curr.type === "cardio") {
+//       acc.totalDistance = (acc.totalDuration || 0) + curr.duration;
+//     }
+//     return acc;
+//   }, {});
+//   return tallied;
+// });
 
     const Workout = mongoose.model("Workout", workoutSchema);
 
